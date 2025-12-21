8b07f75b804e9ea4779b4d8cf83ad47c783b6582
gomes-milindu <gomesmilindu@gmail.com>
2025-12-21 16:04:20 +0530

    update About us Comment Section and get data from array for Cards

--- src/components/homePage/PopularDestination.jsx
+++ src/components/homePage/PopularDestination.jsx
@@ -2,23 +2,20 @@ import Card1 from "./Card1";
 import FeaturedCarousel from "./FeaturedCarousel";
 import Topic from "./topic";
 
- function PopularDestination(){
-    return(
-            <section className="w-full h-[700px] flex flex-col bg-primary">
-                <Topic topic="Top Destination" subtopic="Popular Destination" margin="mt-20"/>
+function PopularDestination() {
+  return (
+    <section className="w-full h-[700px] flex flex-col bg-primary">
+      <Topic
+        topic="Top Destination"
+        subtopic="Popular Destination"
+        margin="mt-20"
+      />
 
-                <div className="w-full mt-15 flex items-center justify-center">
-                    
-                    {/* <Card1  />
-                    <Card1 hid="hidden" blo="sm:block" />
-                    <Card1 hid="hidden" blo="sm:block" />
-                    <Card1 hid="hidden" blo="lg:block" />
-                    <Card1 hid="hidden" blo="lg:block" /> */}
+      <div className="w-full mt-15 flex items-center justify-center">
+        <FeaturedCarousel />
+      </div>
+    </section>
+  );
+}
 
-                    <FeaturedCarousel />
-                </div>
-            </section>
-    )
- }
-
- export default PopularDestination
+export default PopularDestination;
